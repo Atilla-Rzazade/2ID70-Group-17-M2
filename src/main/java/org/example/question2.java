53aa81d720e934b5379ac22b4fe754526e7f6aa8
Doodshoofdaapje <97123208+Doodshoofdaapje@users.noreply.github.com>
2024-03-20 15:36:43 +0100

    Added comments and got rid of edge case

--- src/main/java/org/example/question2.java
+++ src/main/java/org/example/question2.java
@@ -12,40 +12,50 @@ import static org.apache.spark.sql.functions.monotonically_increasing_id;
 
 public class question2 {
     public static void solution(SparkSession spark, JavaRDD<String> eventsRDD, JavaRDD<String> eventTypesRDD) {
+        
+        // Schema for event dataframe
         StructType eventsSchema = new StructType()
                 .add("seriesid", DataTypes.IntegerType)
                 .add("timestamp", DataTypes.IntegerType)
                 .add("eventid", DataTypes.IntegerType);
 
+        // We create a dataframe from the RDD by mapping every line to a Row object
         Dataset<Row> eventsDataset = spark.createDataFrame(eventsRDD.map(row -> {
             String[] parts = row.split(",");
             return RowFactory.create(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
         }), eventsSchema);
+
+        // Sort on series and timestamp in order to get sequences
         eventsDataset = eventsDataset.sort("seriesid", "timestamp");
+
+        // Add a unique id for joining the tables
         eventsDataset = eventsDataset.withColumn("id", monotonically_increasing_id());
         
+        // Schema for eventtypes
         StructType eventTypesSchema = new StructType()
         .add("eventid", DataTypes.IntegerType)
         .add("eventtypeid", DataTypes.IntegerType);
 
+        // We create a dataframe from the RDD by mapping every line to a Row object
         Dataset<Row> eventTypesDataset = spark.createDataFrame(eventTypesRDD.map(row -> {
             String[] parts = row.split(",");
             return RowFactory.create(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
         }), eventTypesSchema);
 
+        // Query for 2.1
         eventsDataset.createOrReplaceTempView("events");
         Dataset<Row> resultEvents = spark.sql(
             "SELECT COUNT(*) " +
             "FROM events e " +
-            "JOIN events p ON e.id = (p.id + 1) " +
-            "JOIN events n ON e.id = (n.id - 1) " + 
+            "JOIN events p ON e.id = (p.id + 1) AND e.seriesid = p.seriesid " +
+            "JOIN events n ON e.id = (n.id - 1) AND e.seriesid = p.seriesid " + 
             "WHERE p.eventid = 109 AND e.eventid = 145 AND n.eventid = 125"
         );
 
-        //resultEvents.show();
         Row resultRowEvent = resultEvents.first();
         long q21 = resultRowEvent.getAs(0);
 
+        // Query for 2.2, same as 2.1 except we map first
         eventTypesDataset.createOrReplaceTempView("eventtypes");
         Dataset<Row> resultTypes = spark.sql(
             "WITH mappedEvents(seriesid, timestamp, eventtypeid, id) AS (SELECT e.seriesid, e.timestamp, t.eventtypeid, e.id " +
@@ -53,12 +63,11 @@ public class question2 {
 
             "SELECT COUNT(*) " +
             "FROM mappedEvents e " +
-            "JOIN mappedEvents p ON e.id = (p.id + 1) " +
-            "JOIN mappedEvents n ON e.id = (n.id - 1) " +
+            "JOIN mappedEvents p ON e.id = (p.id + 1) AND e.seriesid = p.seriesid " +
+            "JOIN mappedEvents n ON e.id = (n.id - 1) AND e.seriesid = p.seriesid " +
             "WHERE p.eventtypeid = 2 AND e.eventtypeid = 11 AND n.eventtypeid = 6"
         );
-        
-        //resultType.show();
+
         Row resultRowType = resultTypes.first();
         long q22 = resultRowType.getAs(0);
         
